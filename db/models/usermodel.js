9f47d8841b6a490948110bbe20b7a070871cea0a
Jay patel <jayspatel190506@gmail.com>
2025-03-24 20:42:36 +0530

    password encryption added

--- db/models/usermodel.js
+++ db/models/usermodel.js
@@ -1,15 +1,41 @@
-import mongoose from "mongoose"
-const userschema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:true
-    },
-    email:{
-        type:String
-    },
-    password:{
-        type:String
-    }
-},{timestamps:true})
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
-export const User = mongoose.models.User || mongoose.model('User', userschema);
+const userschema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+// Hash the password before saving
+userschema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Method to compare entered password with hashed password
+userschema.methods.comparePassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+export const User = mongoose.models.User || mongoose.model("User", userschema);
